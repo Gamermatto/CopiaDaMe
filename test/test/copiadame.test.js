35ca51823f376db3800ac8a4f3cb88204b5be7ff
Gamermatto <gamermatto@icloud.com>
2024-09-09 17:33:55 +0200

    Fix bugs

--- test/test/copiadame.test.js
+++
@@ -1,8 +0,0 @@
-// test/copiadame.test.js
-const assert = require('assert');
-
-describe('Example Test', function() {
-  it('should pass', function() {
-    assert.strictEqual(1 + 1, 2);
-  });
-});
